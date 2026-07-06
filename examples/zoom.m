a23ea84e3b1342f3782cb3c63a5217407eebeebf
Suyash Subodh Shirsat <suyashshirsat8@gmail.com>
2026-07-06 11:48:50 +0530

    Add files via upload
    Zooming using interpolation

---
+++ examples/zoom.m
@@ -0,0 +1,33 @@
+﻿clc;
+clear all;
+
+f=[255 200 234 255 255; 254 223 255 0 255; 200 224 190 145 200; 120 200 130 120 125; 123 200 200 200 200];
+[row,col]=size(f);
+
+% We loop up to row-1 and col-1 to avoid "Out of Bounds"
+for i = 1:row-1
+    for j = 1:col-1
+        x = 2*i - 1;
+        y = 2*j - 1;
+        
+        % 1. The Original Pixel
+        g(x, y) = f(i, j);
+        
+        % 2. Horizontal Interpolation (Right neighbor)
+        g(x, y+1) = (f(i, j) + f(i, j+1)) / 2;
+        
+        % 3. Vertical Interpolation (Bottom neighbor)
+        g(x+1, y) = (f(i, j) + f(i+1, j)) / 2;
+        
+        % 4. Center Interpolation (Average of all 4)
+        g(x+1, y+1) = (f(i, j) + f(i+1, j) + f(i, j+1) + f(i+1, j+1)) / 4;
+    endfor
+endfor
+
+% 5. Handle the very last row and column (the "edges")
+% Copy the last row/column of f to the last row/column of g
+
+% Optional: Fill the tiny gaps in the last row/col if needed
+% but for a 9x9 result, the above covers the core logic.
+figure(1),imshow(uint8(f))
+figure(2),imshow(uint8(g))
